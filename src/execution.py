68c6dc7bebe8e3a855f75c77048673b79d49dc7d
Dentalmovilr4 <115885210+Dentalmovilr4@users.noreply.github.com>
2026-04-13 22:40:48 -0500

    Actualizar execution.py

--- src/execution.py
+++ src/execution.py
@@ -1,14 +1,18 @@
 from exchange import Exchange
+from risk_management import RiskManager
 
 class Executor:
     def __init__(self):
         self.exchange = Exchange()
+        self.risk = RiskManager()
 
-    def execute(self, signal):
+    def execute(self, signal, price):
+        qty = self.risk.calculate_position_size(price)
+
         if signal == "BUY":
-            print("🟢 Ejecutando BUY en Bybit")
-            self.exchange.market_buy()
+            print(f"🟢 BUY {qty}")
+            self.exchange.place_order("Buy", qty, price)
 
         elif signal == "SELL":
-            print("🔴 Ejecutando SELL en Bybit")
-            self.exchange.market_sell()
+            print(f"🔴 SELL {qty}")
+            self.exchange.place_order("Sell", qty, price)
